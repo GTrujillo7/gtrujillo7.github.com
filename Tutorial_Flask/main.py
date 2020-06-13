c296a722e6375a4288792c7eb396712f502d06e8
TheGaboek <perezsaul10@gmail.com>
2020-06-12 22:16:01 -0700

    Commit 4: Uso de ciclos y condiciones
    {{% if fulanito %}}
    
    {{% else %}}
    
    {{% endif %}}
    
    {{% for persona in personas %}}
    
    {{ % endfor %}}

--- Tutorial_Flask/main.py
+++ Tutorial_Flask/main.py
@@ -7,10 +7,13 @@ app = Flask(__name__)
 def index():
     name= "Jaime"
     lastname="Lopez"
+    is_premium = True
+    courses = ["Python", "Ruby","Java", "Elixir"]
 
     return render_template("index.html", username=name,
-                                         userlast=lastname)
-
+                                         userlast=lastname,
+                                         is_premium=is_premium,
+                                         courses=courses)
 if __name__ == '__main__':
     app.run(debug=True, port=9000)
 
