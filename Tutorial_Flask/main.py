b995955427a1ba3ef59dac6819d202dc03742841
TheGaboek <perezsaul10@gmail.com>
2020-06-12 22:02:02 -0700

    Commit 3: Pasando Datos
    {{ }}

--- Tutorial_Flask/main.py
+++ Tutorial_Flask/main.py
@@ -5,7 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    name= "Jaime"
+    lastname="Lopez"
+
+    return render_template("index.html", username=name,
+                                         userlast=lastname)
 
 if __name__ == '__main__':
     app.run(debug=True, port=9000)
