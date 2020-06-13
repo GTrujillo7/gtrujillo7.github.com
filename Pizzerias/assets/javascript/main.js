ae6d2bc52c051de3b8693f8544f3f056c8564b28
TheGaboek <perezsaul10@gmail.com>
2020-06-12 18:41:28 -0700

    Agregando Proyecto Pizzeria

---
+++ Pizzerias/assets/javascript/main.js
@@ -0,0 +1,31 @@
+/*
+//DOM
+
+/*let links = document.querySelectorAll("a");
+
+links.forEach(function(link) {
+    console.log(links);
+}); */
+
+/*let celdas = document.querySelectorAll("td");
+
+celdas.forEach(function(td){
+    td.addEventListener("click", function(){
+        console.log("Click");
+    })
+}); */
+
+// Obtener los elementos de la clase .close
+//let links = document.querySelectorAll(".close");
+
+// Recorrerlos
+/*
+links.forEach(function(link){
+    // Agregar un evento click a cada uno de ellos
+    link.addEventListener("click",function(evento){
+        evento.preventDefault();
+        console.log(":)");
+    });
+}); 
+
+*/
