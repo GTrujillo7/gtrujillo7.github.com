147d83333d5e639c37ca946e48d70556b6eba00c
TheGaboek <perezsaul10@gmail.com>
2020-07-22 21:58:50 -0700

    Optimizando el codigo y evitar sobrecarga de peticiones HTTP
    Optimizando la aplicacion

--- peliculas/peliculasapp/lib/src/providers/peliculas_providers.dart
+++ peliculas/peliculasapp/lib/src/providers/peliculas_providers.dart
@@ -12,6 +12,7 @@ class PeliculasProvider {
   String _language = "es-ES";
 
   int _popularesPage = 0;
+  bool _cargando = false;
 
   List<Pelicula> _populares = new List();
 
@@ -53,6 +54,9 @@ class PeliculasProvider {
 
   // Metodo 2: Utilizando la optimizacion de codigo con _procesarRespuesta
   Future<List<Pelicula>> getPopulares() async {
+    if (_cargando) return [];
+
+    _cargando = true;
     _popularesPage++;
 
     final url = Uri.https(_url, "3/movie/popular", {
@@ -66,6 +70,7 @@ class PeliculasProvider {
     _populares.addAll(resp);
     popularesSink(_populares);
 
+    _cargando = false;
     return resp;
   }
 }
