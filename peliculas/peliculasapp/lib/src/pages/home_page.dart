012c1fa0d98f804549ba3cba4e96cbcd6dac5d4d
TheGaboek <perezsaul10@gmail.com>
2020-07-23 17:07:46 -0700

    Creacion de search Delegate de Manera local
    Aun Falta conexion con API Peliculas

--- peliculas/peliculasapp/lib/src/pages/home_page.dart
+++ peliculas/peliculasapp/lib/src/pages/home_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:peliculasapp/src/pages/widgets/card_swiper_widget.dart';
 import 'package:peliculasapp/src/pages/widgets/movie_horizontal.dart';
 import 'package:peliculasapp/src/providers/peliculas_providers.dart';
+import 'package:peliculasapp/src/search/search_delegate.dart';
 
 class HomePage extends StatelessWidget {
   final peliculasProvider = new PeliculasProvider();
@@ -19,7 +20,12 @@ class HomePage extends StatelessWidget {
           actions: <Widget>[
             IconButton(
               icon: Icon(Icons.search),
-              onPressed: () {},
+              onPressed: () {
+                showSearch(
+                  context: context,
+                  delegate: DataSearch(),
+                );
+              },
             )
           ],
         ),
---
+++ peliculas/peliculasapp/lib/src/search/search_delegate.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class DataSearch extends SearchDelegate {
+  String seleccion = "";
+
+  final peliculas = ["los huevos", "Superman", "superchica", "otrasmas"];
+
+  final peliculasRecientes = ["spiderman", "Capitan AMerica"];
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    // Las acciones de nuestro APPBAR
+    return [
+      IconButton(
+        icon: Icon(Icons.clear),
+        onPressed: () {
+          query = "";
+        },
+      )
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    // Icono a la izquierda del APPBAR
+    return IconButton(
+        icon: AnimatedIcon(
+          icon: AnimatedIcons.menu_arrow,
+          progress: transitionAnimation,
+        ),
+        onPressed: () {
+          close(context, null);
+        });
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    // Crea los resultados que vamos a mostrar
+    return Center(
+      child: Container(
+        height: 100.0,
+        width: 100.0,
+        color: Colors.blueAccent,
+        child: Text(seleccion),
+      ),
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    // Son las sugerencias que aparecen cuando la persona escribe
+
+    final listaSugerida = (query.isEmpty)
+        ? peliculasRecientes
+        : peliculas
+            .where((p) => p.toLowerCase().startsWith(query.toLowerCase()))
+            .toList();
+
+    return ListView.builder(
+      itemCount: listaSugerida.length,
+      itemBuilder: (context, i) {
+        return ListTile(
+          leading: Icon(Icons.movie),
+          title: Text(listaSugerida[i]),
+          onTap: () {
+            seleccion = listaSugerida[i];
+            showResults(context);
+          },
+        );
+      },
+    );
+  }
+}
